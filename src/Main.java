c2cd412781f3b8e44c256db14e6ef5fa3c7b4187
anagalit <ana.galit1@gmail.com>
2022-09-27 12:14:42 +0200

    Add Hallo Ira message

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Hallo hi Welt!");
+        System.out.println("Hallo Ira!");
     }
 }
