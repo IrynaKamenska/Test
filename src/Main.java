0c4120d4c8ea5636c2fb3cae9978ccd3f65171a8
davidskottke <davidskottke@googlemail.com>
2022-09-27 12:28:02 +0200

    add huhu

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Hallo hi Welt!");
-        System.out.println("huh u ");
+        System.out.println("huhu");
+        System.out.println("huhu1");
     }
 }
