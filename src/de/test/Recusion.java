63b9eeb026220ec0741e531313f0d9568361e963
Iryna <pshonka@gmail.com>
2022-09-28 19:21:19 +0200

    add example method for recursion

---
+++ src/de/test/Recusion.java
@@ -0,0 +1,16 @@
+package de.test;
+
+public class Recusion {
+    public static void main(String[] args) {
+        sayHi(3);
+    }
+
+
+    public static void sayHi(int count){
+        System.out.println("Hi");
+        if(count <= 1) {
+            return;
+        }
+        sayHi(count - 1);
+    }
+}
---
+++ src/de/test/While.java
@@ -0,0 +1,17 @@
+package de.test;
+
+public class While {
+    public static void main(String[] args) {
+        int count =0;
+        while (count <= 10) {
+            System.out.println(count++);
+        }
+
+
+       // for
+
+        for (int i = 0; i <= 10; i++) {
+            System.out.print(i);
+        }
+    }
+}
